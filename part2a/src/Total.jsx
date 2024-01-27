b24a0997c2be3e815c69b8a3ad23e4db168a81cd
Sanwicklith <kcvmd22@gmail.com>
2024-01-27 13:11:39 +0200

    Exercise_2.3

--- part2a/src/Total.jsx
+++ part2a/src/Total.jsx
@@ -1,9 +1,17 @@
 const Total = ({ course }) => {
   const parts = course.parts;
-  let totalExercises = 0;
-  parts.forEach((part) => {
-    totalExercises += part.exercises;
+  //   let totalExercises = 0;
+  //   parts.forEach((part) => {
+  //     totalExercises += part.exercises;
+  //   });
+  let exercises = [];
+  parts.forEach((element) => {
+    exercises.push(element.exercises);
   });
+
+  let totalExercises = exercises.reduce((acc, cv) => {
+    return acc + cv;
+  }, 0);
   return (
     <div>
       <h3>Total of {totalExercises} exercises</h3>
