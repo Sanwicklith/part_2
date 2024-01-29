3aae376ea70587cc818b6dc952d3e00d838ef3ca
Sanwicklith <kcvmd22@gmail.com>
2024-01-29 19:20:44 +0200

    2.11

--- part2_phonebook/src/App.jsx
+++ part2_phonebook/src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPersons = async () => {
+      try {
+        const res = await axios.get("http://localhost:3001/persons");
+        const notes = res.data;
+        setPersons(notes);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchPersons();
+  }, []);
 
   const addName = (name, number) => {
     if (
@@ -36,6 +50,14 @@ const App = () => {
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(search.toLowerCase())
   );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div>
